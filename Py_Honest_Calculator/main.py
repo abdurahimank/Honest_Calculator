8971ab9a711a09a72c6ceaf78805542bc08608c8
Abdurahiman K <laluvga@gmail.com>
2021-12-07 08:53:04 +0530

    first final code

--- Py_Honest_Calculator/main.py
+++ Py_Honest_Calculator/main.py
@@ -1,4 +1,23 @@
-memory = 0
+def check(v1, v2, v3):
+    msg = ""
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg = msg + " ... lazy"
+    if (v1 == 1 or v2 == 1) and v3 == "*":
+        msg = msg + " ... very lazy"
+    if (v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-"):
+        msg = msg + " ... very, very lazy"
+    if msg != "":
+        msg = "You are" + msg
+        print(msg)
+
+
+def is_one_digit(v):
+    if (v > -10) and (v < 10) and float(v) % 1 == 0:
+        return True
+    return False
+
+
+memory = "0"
 while True:
     print("Enter an equation")
     calc = input()
@@ -21,6 +40,7 @@ while True:
     else:
         x = float(x)
         y = float(y)
+        check(x, y, oper)
         if oper == "+":
             result = (x + y)
         elif oper == "-":
@@ -37,8 +57,27 @@ while True:
         while True:
             answer = input("Do you want to store the result? (y / n):")
             if answer == "y":
-                memory = result
-                break
+                if is_one_digit(result):
+                    msg = {"msg_10": "Are you sure? It is only one digit! (y / n)",
+                           "msg_11": "Don't be silly! It's just one number! Add to the memory? (y / n)",
+                           "msg_12": "Last chance! Do you really want to embarrass yourself? (y / n)"}
+                    msg_index = 10
+                    while True:
+                        answer = input(msg["msg_" + str(msg_index)])
+                        if answer == "y":
+                            if msg_index < 12:
+                                msg_index += 1
+                            else:
+                                memory = result
+                                break
+                        elif answer == "n":
+                            break
+                        else:
+                            continue
+                    break
+                else:
+                    memory = result
+                    break
             elif answer == "n":
                 break
             else:
--- Py_Honest_Calculator/test.py
+++ Py_Honest_Calculator/test.py
@@ -1,3 +1,5 @@
-word = input("type a word: ")
-print(word.replace(".", "").isdigit())
-# print(word.replace('.', ''))
+msg = {"msg_10": "Are you sure? It is only one digit! (y / n)",
+       "msg_11": "Don't be silly! It's just one number! Add to the memory? (y / n)",
+       "msg_12": "Last chance! Do you really want to embarrass yourself? (y / n)"}
+index = 12
+print(msg["msg_" + str(index)])

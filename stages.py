a9cef4e13cec95c22409b613b9232272da3517dd
ABDURAHIMAN K <laluvga@gmail.com>
2023-04-24 17:15:52 +0530

    Stage 1/5: Data collection

---
+++ stages.py
@@ -0,0 +1,13 @@
+# Stage 1/5: Data collection
+msg_0 = "Enter an equation\n"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+while True:
+    x, oper, y = input(msg_0).split()
+    if x.replace(".", "").isdigit() and y.replace(".", "").isdigit():
+        if oper not in ["+", "-", "*", "/"]:
+            print(msg_2)
+        else:
+            break
+    else:
+        print(msg_1)
